407a165a460e496698c13baae5de6117b9f828c1
Santa94 <45481894+Santa94@users.noreply.github.com>
2018-12-22 17:11:46 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-function animal(obj){
-  var animal = {name:"dog",legs:4,color:"white"};
-  return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
+function animal(obj) {
+    return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
 }
